f56b8a9a6ad3132bebf5ebd6a43efdd0b3533c37
Ololade Sanya <ololadetimileyin3@gmail.com>
2025-01-02 12:13:56 +0100

    made some other changes to the native wind setup

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className=" font-bold text-lg my-10">Welcome to Realest</Text>
       <Link href="/sign-in">Signin</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
