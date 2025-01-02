fdccb894a470ae6674af7a8897bfd772fd043184
Ololade Sanya <ololadetimileyin3@gmail.com>
2025-01-02 12:48:44 +0100

    setup fonts and colors and also setup image declarations for the image constant to fix the errors

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className=" font-bold text-lg my-10">Welcome to Realest</Text>
+      <Text className=" font-bold my-10 font-rubik text-3xl">Welcome to Realest</Text>
       <Link href="/sign-in">Signin</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import "./globals.css";
+import { useFonts } from "expo-font"
+import { useEffect } from "react";
 
 export default function RootLayout() {
+
+  const [fontLoaded] = useFonts({
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf")
+  })
+
+  useEffect(() => {
+    if (fontLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded])
+
+  if (!fontLoaded) {
+    return null;
+  }
   return <Stack />;
 }
