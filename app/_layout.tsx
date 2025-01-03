e1f49762b4854044f1ec7eac6684b1389dccaed2
Ololade Sanya <ololadetimileyin3@gmail.com>
2025-01-03 03:23:26 +0100

    implimented the login functionality to a very good extent

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { SplashScreen, Stack } from "expo-router";
 import "./globals.css";
 import { useFonts } from "expo-font"
 import { useEffect } from "react";
+import GlobalProvider from "@/lib/global-provider";
 
 export default function RootLayout() {
 
@@ -23,5 +24,11 @@ export default function RootLayout() {
   if (!fontLoaded) {
     return null;
   }
-  return <Stack />;
+  return (
+    <GlobalProvider>
+      <Stack screenOptions={{
+        headerShown: false
+      }} />;
+    </GlobalProvider>
+  )
 }
