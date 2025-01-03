d2edd3f793d5405d5acec614c35612c8fd16ea3f
Ololade Sanya <ololadetimileyin3@gmail.com>
2025-01-03 06:03:44 +0100

    created some card component and still creatin other reusable components

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,3 +1,5 @@
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
@@ -32,7 +34,32 @@ export default function Index() {
           </View>
 
         </View>
+
+        <View className=" flex flex-row gap-5 mt-5">
+          <FeaturedCard />
+          <FeaturedCard />
+        </View>
+        <View className=" my-5">
+          <View className=" flex flex-row justify-between items-center">
+            <Text className=" text-xl font-rubik-bold text-black-300">
+              Our Recommendation
+            </Text>
+            <TouchableOpacity>
+              <Text className=" text-base font-rubik-bold text-primary-300">See All</Text>
+            </TouchableOpacity>
+          </View>
+          <Filters />
+          <View className=" flex flex-row gap-5 mt-5 ">
+            <Card />
+            <Card />
+
+          </View>
+
+        </View>
+
+
       </View>
+
 
 
 
